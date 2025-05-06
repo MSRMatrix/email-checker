5b92e6e57a01675b03d97015cf887339a40dfb57
TwinMine <michaelrolo11@gmail.com>
2025-05-06 14:51:43 +0200

    smartphone style

--- src/components/dashboard/footer/Footer.jsx
+++ src/components/dashboard/footer/Footer.jsx
@@ -5,20 +5,18 @@ import { Theme } from "../../context/Context";
 const Footer = () => { 
     const { theme, setTheme } = useContext(Theme);
     return(
-        <div className={`footer ${theme.anything}`}>
+        <div className={`footer`}>
         <h2>Note: </h2>
         <p>
           Sometimes, a test or tool may indicate that an email address is valid,
-          but it might still have issues.{" "}
+          but it might still have issues.
         </p>
         <p>
-          {" "}
           This can be because the test only checks certain aspects (like format
           or syntax) and not all possible factors that contribute to an email's
           validity.
         </p>
         <p>
-          {" "}
           For example, the domain might not exist, or the mail server might be
           unreachable.
         </p>
