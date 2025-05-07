51e8e925c6355e8b17a6dfdb1c17d7d5a50ecf1b
TwinMine <michaelrolo11@gmail.com>
2025-05-07 17:29:13 +0200

    smartphone test

--- src/components/dashboard/section/formComponent/FormComponent.jsx
+++ src/components/dashboard/section/formComponent/FormComponent.jsx
@@ -10,17 +10,24 @@ function FormComponent() {
     e.preventDefault();
     const emails = e.target.elements.textarea.value.trim().split(/\s+/);
 
+    const uniqueEmails = emails.reduce((acc, email) => {
+      if (!acc.includes(email)) {
+        acc.push(email);
+      }
+      return acc;
+    }, []);
+
+    
     try {
       const response = await Promise.all(
-        emails.map(async (email) => {
+        uniqueEmails.map(async (email) => {
           const response = await fetch(`${URL}${email}`);
           const data = await response.json();
           return { email, ...data };
         })
       );
         setResult(response)
-        console.log(response);
-        
+        e.target.reset()
         return console.log(`Informations ready!`);
     } catch (error) {
       console.error("Error checking emails:", error);
@@ -33,6 +40,7 @@ function FormComponent() {
       <fieldset>
       <textarea required name="textarea" placeholder="Enter emails separated by space or newlines" className={theme.anything} />
       <button type="submit">Check</button>  
+      {/* <button type="reset">Clear</button> */}
       </fieldset>
     </form>
   );
